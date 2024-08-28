d7a300e5b66864ead6b1b3960b830e7309bd12bd
5had0w-SS-P4k4-e <126048408+5had0w-SS-P4k4-e@users.noreply.github.com>
2024-08-28 21:58:50 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,20 +4,20 @@
 TEMP_PYTHON_FILE="/tmp/setuper.py"
 TEMP_TASKS_FILE="/tmp/tasks.json"
 TEMP_KEYBINDINGS_FILE="/tmp/keybindings.json"
+LOG_FILE="/tmp/setuper_output.log"
 
 # URL файлов с GitHub
-TEMP_PYTHON_URL="https://raw.github.com/5had0w-SS-P4k4-e/SetupTasksForVisualStudioC/blob/main/setuper.py"
+PYTHON_URL="https://raw.githubusercontent.com/5had0w-SS-P4k4-e/SetupTasksForVisualStudioC/main/setuper.py"
 TASKS_URL="https://raw.githubusercontent.com/5had0w-SS-P4k4-e/SetupTasksForVisualStudioC/main/tasks.json"
 KEYBINDINGS_URL="https://raw.githubusercontent.com/5had0w-SS-P4k4-e/SetupTasksForVisualStudioC/main/keybindings.json"
 
 # Загружаем файлы
-curl -s "$TEMP_PYTHON_URL" -o "$TEMP_PYTHON_FILE"
+curl -s "$PYTHON_URL" -o "$TEMP_PYTHON_FILE"
 curl -s "$TASKS_URL" -o "$TEMP_TASKS_FILE"
 curl -s "$KEYBINDINGS_URL" -o "$TEMP_KEYBINDINGS_FILE"
 
 # Запускаем Python-скрипт
-python3 "$TEMP_PYTHON_FILE" "$TEMP_TASKS_FILE" "$TEMP_KEYBINDINGS_FILE"
+python3 "$TEMP_PYTHON_FILE" "$TEMP_TASKS_FILE" "$TEMP_KEYBINDINGS_FILE" | tee "$LOG_FILE"
 
 # Удаляем временные файлы
-rm "$TEMP_PYTHON_FILE" "$TEMP_TASKS_FILE" "$TEMP_KEYBINDINGS_FILE"
-
+rm "$TEMP_PYTHON_FILE" "$TEMP_TASKS_FILE" "$TEMP_KEYBINDINGS_FILE" "$LOG_FILE"
